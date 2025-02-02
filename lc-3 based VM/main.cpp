c316664213cbe897280a6ddc1c93373383460244
unknown <kabaloofri>
2025-02-02 10:04:13 +0200

    finished all the instructions

--- lc-3 based VM/main.cpp
+++ lc-3 based VM/main.cpp
@@ -17,13 +17,13 @@ int NOT(uint16_t instruction);
 int BR(uint16_t instruction);
 int JMP(uint16_t instruction);
 int JSR(uint16_t instruction);
-int LD(uint16_t instrutcion);
+int LD(uint16_t instruction);
 int LDI(uint16_t instruction);
 int LDR(uint16_t instruction);
 int LEA(uint16_t instruction);
-int ST(uint16_t instrutcion);
-int STI(uint16_t instrutcion);
-int STR(uint16_t instrutcion);
+int ST(uint16_t instruction);
+int STI(uint16_t instruction);
+int STR(uint16_t instruction);
 int TRAP(uint16_t instruction);
 int RES(uint16_t instruction); //maybe switch these 2 to psh and pop from stack
 int RTI(uint16_t instruction);
@@ -175,11 +175,16 @@ void update_flags(uint16_t r)
     }
 }
 
-// adds 2 registers or a register and an immedate and stores the result in another register.
-// Usage:
-// ADD destReg firstReg secondReg/Immediate
-// Example:
-// ADD R0 R1 5 - adds R1 and 5 and stores them in R0
+/*
+If bit [5] is 0, the second source operand is obtained from SR2. If bit [5] is 1, the
+second source operand is obtained by sign-extending the imm5 field to 16 bits.
+In both cases, the second source operand is added to the contents of SR1 and the
+result stored in DR. The condition codes are set, based on whether the result is
+negative, zero, or positive.
+Examples
+ADD R2, R3, R4 ; R2 <- R3 + R4
+ADD R2, R3, #7 ; R2 <- R3 + 7
+*/
 int ADD(uint16_t instruction)
 {
     /* destination register (DR) */
@@ -206,12 +211,16 @@ int ADD(uint16_t instruction)
 
 
 
-// does bitwise and between 2 registers or a register and an immediate
-// Usage:
-// AND destReg reg reg/5BitImmediate
-// Example:
-// AND R0 R1 R2 - does bitwise AND between R1 and R2 and stores in R0
-// AND R0 R1 10101 - does bitwise AND between R1 and 10101, and stores in R0
+/*
+If bit [5] is 0, the second source operand is obtained from SR2. If bit [5] is 1,
+the second source operand is obtained by sign-extending the imm5 field to 16
+bits. In either case, the second source operand and the contents of SR1 are bitwise ANDed, and the result stored in DR. The condition codes are set, based on
+whether the binary value produced, taken as a 2’s complement integer, is negative,
+zero, or positive.
+Examples
+AND R2, R3, R4 ;R2 <- R3 AND R4
+AND R2, R3, #7 ;R2 <- R3 AND 7
+*/
 int AND(uint16_t instruction)
 {
     uint16_t destReg = (instruction >> 9) & 0b111;
@@ -235,11 +244,12 @@ int AND(uint16_t instruction)
 
 
 
-// does bitwise NOT on register or and stores result in another register
-// Usage:
-// NOT destReg reg
-// Example:
-// NOT R0 R1 - does bitwise NOT on R1 and stores in R0
+/*
+The bit-wise complement of the contents of SR is stored in DR. The condition codes are set, based on whether the binary value produced, taken as a 2’s
+complement integer, is negative, zero, or positive.
+Example
+NOT R4, R2 ; R4 <- NOT(R2)
+*/
 int NOT(uint16_t instruction)
 {
     uint16_t destReg = (instruction >> 9) & 0b111;
@@ -251,11 +261,15 @@ int NOT(uint16_t instruction)
     return GOOD;
 }
 
-// branch- like LOOP in 8086, jumps to memory location (label) if the flag(s) specified were on
-// Usage: 
-// BR label/memoryLocation
-// Example:
-// BRn LOOP(101010101) - if N flag is on then jump to LOOP(101010101)
+/*
+The condition codes specified by the state of bits [11:9] are tested. If bit [11] is
+set, N is tested; if bit [11] is clear, N is not tested. If bit [10] is set, Z is tested, etc.
+If any of the condition codes tested is set, the program branches to the location
+specified by adding the sign-extended PCoffset9 field to the incremented PC.
+Examples
+BRzp LOOP ; Branch to LOOP if the last result was zero or positive.
+BR NEXT ; Unconditionally branch to NEXT.
+*/
 int BR(uint16_t instruction)
 {
     uint16_t flags = (instruction >> 9) & 0b111;
@@ -270,12 +284,17 @@ int BR(uint16_t instruction)
 
 
 
-// jumps to a certain location in memory
-// Usage:
-// JMP register
-// Example:
-// JMP R0 - jumps to the location in the memory that is in R0
-// special case: JMP R7 is .RET (see .h file in regs)
+/*
+The program unconditionally jumps to the location specified by the contents of
+the base register. Bits [8:6] identify the base register.
+Examples
+JMP R2 ; PC <- R2
+RET ; PC <- R7
+Note
+The RET instruction is a special case of the JMP instruction. The PC is loaded
+with the contents of R7, which contains the linkage back to the instruction
+following the subroutine call instruction.
+*/
 int JMP(uint16_t instruction)
 {
     uint16_t destReg = (instruction >> 6) & 0b111;
@@ -284,12 +303,17 @@ int JMP(uint16_t instruction)
 }
 
 
-//jump to subroutine- jumps to a subroutine (function) - puts pc curr location in R7 and jumps to location
-// Usage:
-// JSR label/regiter
-// Example:
-// JSR R0 - jumps to R0
-// JSR label(101010101) - jumps to label(101010101)
+/*
+First, the incremented PC is saved in R7. This is the linkage back to the calling
+routine. Then the PC is loaded with the address of the first instruction of the
+subroutine, causing an unconditional jump to that address. The address of the
+subroutine is obtained from the base register (if bit [11] is 0), or the address is
+computed by sign-extending bits [10:0] and adding this value to the incremented
+PC (if bit [11] is 1).
+Examples:
+JSR QUEUE ; Put the address of the instruction following JSR into R7 ; Jump to QUEUE.
+JSRR R3 ; Put the address following JSRR into R7; Jump to the address contained in R3.
+*/
 int JSR(uint16_t instruction)
 {
     uint16_t jmpToSubRFlag = (instruction >> 11) & 0b1;
@@ -310,12 +334,34 @@ int JSR(uint16_t instruction)
 
 
 
-// Load Indirect: loads a vaule from a memory location to a register.
-// Usage:
-// LDI destReg 9bitMemoryLocation
-// Example:
-// LDI R0 101010101 - puts the value of the location in the memory of 101010101 
-// so if in 101010101 there is 5, it will put whatever is in memory 5 in R0
+/*
+An address is computed by sign-extending bits [8:0] to 16 bits and adding this
+value to the incremented PC. The contents of memory at this address are loaded
+into DR. The condition codes are set, based on whether the value loaded is
+negative, zero, or positive.
+Example:
+LD R4, VALUE ; R4 <- mem[VALUE]
+*/
+int LD(uint16_t instruction)
+{
+    uint16_t destReg = (instruction >> 9) & 0b111;
+    uint16_t offset = sign_extend(instruction & 0b111111111, 9);
+    
+    reg[destReg] = mem_read(reg[R_PC] + offset);
+    update_flags(destReg);
+    return GOOD;
+}
+
+
+
+/*
+An address is computed by sign-extending bits [8:0] to 16 bits and adding this
+value to the incremented PC. What is stored in memory at this address is the
+address of the data to be loaded into DR. The condition codes are set, based on
+whether the value loaded is negative, zero, or positive.
+Example:
+LDI R4, ONEMORE ; R4 <- mem[mem[ONEMORE]]
+*/
 int LDI(uint16_t instruction)
 {
     uint16_t destReg = (instruction >> 9) & 0b111; //destenation register
@@ -323,6 +369,106 @@ int LDI(uint16_t instruction)
 
     reg[destReg] = mem_read(mem_read(reg[R_PC] + pcOffset));
     update_flags(destReg);
+    return GOOD;
+}
+
+
+/*
+An address is computed by sign-extending bits [5:0] to 16 bits and adding this
+value to the contents of the register specified by bits [8:6]. The contents of memory
+at this address are loaded into DR. The condition codes are set, based on whether
+the value loaded is negative, zero, or positive.
+Example:
+LDR R0 R1 -20  ; R0 <- mem[R1 + (-20)]
+*/
+int LDR(uint16_t instruction)
+{
+    uint16_t destReg = (instruction >> 9) & 0b111; //destenation register
+    uint16_t base = (instruction >> 6) & 0b111;
+    uint16_t offset = sign_extend(instruction & 0b111111, 6);
+    
+    reg[destReg] = mem_read(base + offset);
+
+    update_flags(destReg);
+    return GOOD;
+}
+
+
+/*
+An address is computed by sign-extending bits [8:0] to 16 bits and adding this
+value to the incremented PC. This address is loaded into DR. The condition
+codes are set, based on whether the value loaded is negative, zero, or positive.
+Example
+LEA R4, TARGET ; R4 <- address of TARGET.
+*/
+int LEA(uint16_t instruction)
+{
+    uint16_t destReg = (instruction >> 9) & 0b111;
+    uint16_t offset = sign_extend(instruction & 0b111111111, 9);
+
+    reg[destReg] = reg[R_PC] + offset;
+    update_flags(destReg);
+    return GOOD;
+}
+
+
+
+
+/*
+The contents of the register specified by SR are stored in the memory location
+whose address is computed by sign-extending bits [8:0] to 16 bits and adding this
+value to the incremented PC.
+Example:
+ST R4, HERE ; mem[HERE] <- R4
+*/
+int ST(uint16_t instruction)
+{
+    uint16_t srcReg = (instruction >> 9) & 0b111;
+    uint16_t offset = sign_extend(instruction & 0b111111111, 9);
+
+    mem_write(reg[R_PC] + offset, reg[srcReg]);
+    return GOOD;
+}
+
+
+
+
+
+/*
+The contents of the register specified by SR are stored in the memory location
+whose address is obtained as follows: Bits [8:0] are sign-extended to 16 bits and
+added to the incremented PC. What is in memory at this address is the address of
+the location to which the data in SR is stored.
+Example
+STI R4, NOT_HERE ; mem[mem[NOT_HERE]] <- R4
+*/
+int STI(uint16_t instruction)
+{
+    uint16_t srcReg = (instruction >> 9) & 0b111;
+    uint16_t offset = sign_extend(instruction & 0b111111111, 9);
+
+    mem_write(mem_read(reg[R_PC] + offset), reg[srcReg]);
+    return GOOD;
+}
+
+
+
+
+
+/*
+The contents of the register specified by SR are stored in the memory location
+whose address is computed by sign-extending bits [5:0] to 16 bits and adding this
+value to the contents of the register specified by bits [8:6].
+Example
+STR R4, R2, 5 ; mem[R2 + 5] <- R4
+*/
+int STR(uint16_t instruction)
+{
+    uint16_t srcReg = (instruction >> 9) & 0b111;
+    uint16_t baseReg = (instruction >> 6) & 0b111;
+    uint16_t offset = sign_extend(instruction & 0b111111, 6);
+
+    mem_write(reg[baseReg] + offset, reg[srcReg]);
     return GOOD;
 }
 
--- lc-3 based VM/stuff.h
+++ lc-3 based VM/stuff.h
@@ -45,6 +45,17 @@ enum //condition flags (in the R_COND - flags reg)
     FL_NEG = 1 << 2, /* N 100 */
 };
 
+
+enum // trap routines- like mini functions stuff api
+{
+    TRAP_GETC = 0x20,  /* get character from keyboard, not echoed onto the terminal */
+    TRAP_OUT = 0x21,   /* output a character */
+    TRAP_PUTS = 0x22,  /* output a word string */
+    TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal */
+    TRAP_PUTSP = 0x24, /* output a byte string */
+    TRAP_HALT = 0x25   /* halt the program */
+};
+
 enum // all the errors
 {
     GOOD = 0,
